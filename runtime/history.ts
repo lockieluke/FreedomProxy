57050564ca8b3682dd90b97a8567acd3b1fc2722
Sherlock Luk <sherlockluk@outlook.com>
2023-05-31 22:59:23 +0100

    properly intercepting history functions

--- runtime/history.ts
+++ runtime/history.ts
@@ -1,10 +1,24 @@
-import * as _ from "lodash-es";
-
 export default class History {
 
     static interceptHistoryActions() {
-        window.history.pushState = _.constant(void 0);
-        window.history.replaceState = _.constant(void 0);
+        window.history.pushState = new Proxy(window.history.pushState, {
+            apply: (...args) => {
+                window.dispatchEvent(new Event('locationchange'));
+                window.dispatchEvent(new Event('pushstate'));
+                return void 0;
+            }
+        });
+        window.history.replaceState = new Proxy(window.history.replaceState, {
+            apply: (...args) => {
+                window.dispatchEvent(new Event('locationchange'));
+                window.dispatchEvent(new Event('replacestate'));
+                return void 0;
+            }
+        });
+
+        window.addEventListener('popstate', () => {
+            window.dispatchEvent(new Event('locationchange'));
+        });
     }
 
 }
