b61af9528cd3d0cc94c544888bd6e051259a4b0d
Sherlock Luk <sherlockluk@outlook.com>
2023-10-17 20:59:21 +0100

    clean up code

--- web/index.ts
+++ web/index.ts
@@ -1,6 +1,7 @@
 /// <reference path="../shared/navigationApi.d.ts" />
+/// <reference path="./types.d.ts" />
 
-import BareClient, {BareFetchInit, createBareClient} from "@tomphttp/bare-client";
+import {BareClient, createBareClient} from "@tomphttp/bare-client";
 import $ from 'cash-dom';
 import * as async from 'modern-async';
 import App from "./components/Root";
@@ -13,12 +14,12 @@ import {render} from "preact";
 
 document.title = Product.productName;
 
-const serverUrl = 'http://localhost:8080';
+const serverUrl = import.meta.env.VITE_ENDPOINT ?? 'http://localhost:8080';
 $(async () => {
     const extensionResponders = [new CookiePopupBlockerResponder()];
 
     if (Utils.isChromium && window.navigation) {
-        let err, bareClient: BareClient
+        let err, bareClient: BareClient | undefined;
         [err, bareClient] = await to(createBareClient(`${serverUrl}/bare/`));
         if (err)
             console.error("❌ Failed to connect to initialise Bare Client", err);
@@ -26,7 +27,8 @@ $(async () => {
             // @ts-ignore
             window.fetch = (input, init) => {
                 try {
-                    return bareClient.fetch(input, init as BareFetchInit);
+                    // @ts-ignore
+                    return bareClient?.fetch(input, init);
                 } catch (err) {
                     console.error("❌ Failed to fetch", err);
                     throw err;
@@ -39,13 +41,13 @@ $(async () => {
         const iframe = <HTMLIFrameElement>document.getElementById('webview');
         const type = message.type;
 
-        const sendResponse = (response: any) => iframe.contentWindow.postMessage(JSON.stringify(response), '*');
+        const sendResponse = (response: any) => iframe.contentWindow?.postMessage(JSON.stringify(response), '*');
         await async.forEach(extensionResponders, extensionResponder => {
-            if (type.startsWith(extensionResponder.messagePrefix))
+            if (type?.startsWith(extensionResponder.messagePrefix))
                 extensionResponder.onMessage(message, sendResponse);
         });
     });
 
-    render(App(), document.getElementById('app'));
+    render(App(), document.getElementById('app')!);
     console.log("✅ Initialised Preact UI");
 });
--- web/types.d.ts
+++ web/types.d.ts
@@ -1,3 +1,4 @@
+import 'vite/client';
 import Helper from "./helper";
 
 declare global {
