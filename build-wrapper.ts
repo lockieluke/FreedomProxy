94f9fcb9114cd36c8fea93d67337df3503f9d170
Sherlock Luk <sherlockluk@outlook.com>
2024-01-16 18:34:01 +0000

    improve building

--- build-wrapper.ts
+++ build-wrapper.ts
@@ -4,8 +4,9 @@ import * as _ from 'lodash-es';
 const wrapperTemplateHtml = _.template(await fs.readFile('wrapper/index.html', 'utf-8'));
 const webHTML = await fs.readFile('dist/index.html', 'base64');
 const html = wrapperTemplateHtml({
-   title: "Student Portal",
-   url: `data:text/html;base64,${webHTML}`
+    title: "Student Portal",
+    url: `data:text/html;base64,${webHTML}`,
+    html: webHTML
 });
 
 await fs.writeFile('dist/wrapper.html', html);
